ed691d227276d99331c14c59410b6d3c3fb5ecf0
M2K Developments <m2ksoftiez@gmail.com>
2023-07-26 19:30:29 +0200

    update choruses

--- lib/screens/youtube.dart
+++ lib/screens/youtube.dart
@@ -3,6 +3,7 @@ import 'package:kabulachoruses/classes/chorus.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class ScreenYoutube extends StatefulWidget {
+  
   const ScreenYoutube({Key? key}) : super(key: key);
 
   @override
@@ -12,6 +13,7 @@ class ScreenYoutube extends StatefulWidget {
 class _ScreenYoutubeState extends State<ScreenYoutube> {
   @override
   Widget build(BuildContext context) {
+
     final chorus = ModalRoute.of(context)!.settings.arguments as Chorus;
 
     return Scaffold(
